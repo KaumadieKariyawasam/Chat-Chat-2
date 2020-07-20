3d1839c07da283eb9ed0a82cbf9321fbe520c047
Kaumadie Kariyawasam <kkawmadie@gmail.com>
2020-07-20 12:40:26 +0530

    save msg in firebase

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {SafeAreaView, View,Text,TextInput} from 'react-native';
 import styles from '../constants/styles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import User from '../User';
+import firebase from 'firebase'
 
 
 export default class ChatScreen extends React.Component {
@@ -9,19 +12,47 @@ export default class ChatScreen extends React.Component {
            title:navigation.getParam('name',null)
        }
    }
-   state={
-       textMessage:''
-   }
+  constructor(props){
+      super(props);
+      this.state={
+          person:{
+              name:props.navigation.getParam('name'),
+              phone:props.navigation.getParam('phone'),
+          },
+          textMessage:''
+      }
+  }
    handleChange=key=>val=>{
        this.setState({[key]:val})
+   }
+   sendMessage=async()=>{
+       if(this.state.textMessage.length>0){
+           let msgId=firebase.database().ref('messages').child(User.phone).child(this.state.person.phone).push().key;
+            let updates={};
+            let message={
+                message:this.state.textMessage,
+                time:firebase.database.ServerValue.TIMESTAMP,
+                from :User.phone
+            }
+            updates['messages/'+User.phone+'/'+this.state.person.phone+'/'+msgId]=message;
+            updates['messages/'+this.state.person.phone+'/'+User.phone+'/'+msgId]=message;
+            firebase.database().ref().update(updates);
+            this.setState({textMessage:''});
+        }
+
    }
     render() {
         return (
             <SafeAreaView>
+                <View style={{flexDirection:'row',alignItems:'center'}}>
                <TextInput style={styles.input}
                value={this.state.textMessage}
-               onChangeText={this.handleChange('textMsg')}
+               onChangeText={this.handleChange('textMessage')}
                />
+               <TouchableOpacity onPress={this.sendMessage}>
+                   <Text style={styles.btnText}>Send</Text>
+               </TouchableOpacity>
+               </View>
             </SafeAreaView>
         );
     }
